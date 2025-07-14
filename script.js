ec91cbc26d356f154c25a034c0e27ab125d46b34
piyushmukhija9 <piyushmukhija9@gmail.com>
2025-07-14 12:44:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ function speakText() {
       "Lavi, your name means light — but your vibe is full sunshine!",
       "Lavi... you're not just a name, you're a whole vibe!"
     ]
+    "parmeet": [
+    "Parmeet has entered the chat. Decency just hit 100%.",
+    "Parmeet... calm, composed, and carrying a whole lot of grace.",
+    "When Parmeet speaks, even chaos listens silently.",
+    "Parmeet detected. Please switch to polite mode instantly.",
+    "Parmeet... the only person who can make silence feel peaceful.",
+    "Parmeet is here. That’s your daily reminder that class still exists.",
+    "Decency alert: Parmeet has been mentioned. Please behave, everyone!"
+  ]
   };
 
   if (customWords[text]) {
